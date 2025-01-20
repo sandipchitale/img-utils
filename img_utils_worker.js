a1b8491e7188d95153e3ed8d061c6ede79a1a359
Sandip Chitale <sandipchitale@gmail.com>
2025-01-20 03:37:43 -0500

    Copy to clipboard.

--- img_utils_worker.js
+++ img_utils_worker.js
@@ -18,14 +18,13 @@
         }
     });
 
-    chrome.contextMenus.onClicked.addListener(async (info, tab  ) => {
-        chrome.tabs.create({ url: info.srcUrl, active: true },  (tab) => {
+    chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+        chrome.tabs.create({url: info.srcUrl, active: true}, (tab) => {
             chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: async (menuItemId) => {
+                target: {tabId: tab.id},
+                func: async (menuItemId, tabId, windowId) => {
                     let imageElement = document.querySelector("img");
                     if (imageElement) {
-                        console.log(menuItemId);
                         if (menuItemId === 'plus90') {
                             imageElement.style.transform = 'rotate(270deg)';
                         } else if (menuItemId === 'plus180') {
@@ -38,9 +37,46 @@
                             imageElement.style.transform = 'scaleX(-1)';
                         }
                     }
+                    setTimeout(() => {
+                        chrome.runtime.sendMessage({
+                            type: 'rect',
+                            windowId: windowId,
+                            tabId: tabId,
+                            data: imageElement.getBoundingClientRect()
+                        })
+                    }, 0);
                 },
-                args: [info.menuItemId]
+                args: [info.menuItemId, tab.id, tab.windowId]
             });
         });
+    });
+
+    chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+        if (request.type === 'rect') {
+            // Process the message and data
+            let dataUrl = await chrome.tabs.captureVisibleTab(request.windowId, {
+                format: 'png'
+            });
+
+            await chrome.scripting.executeScript({
+                target: {tabId: request.tabId},
+                func: async (dataUrl) => {
+                    let response = await fetch(dataUrl);
+                    let blob = await response.blob();
+                    // Create a ClipboardItem with the image Blob
+                    const clipboardItem = new ClipboardItem({
+                        [blob.type]: blob
+                    });
+
+                    // Write the ClipboardItem to the clipboard
+                    try {
+                        await navigator.clipboard.write([clipboardItem]);
+                    } catch (e) {
+                        alert(`Could not copy ${clipboardItem} ${e}`);
+                    }
+                },
+                args: [dataUrl]
+            });
+        }
     });
 })();
